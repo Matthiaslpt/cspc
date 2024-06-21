387868f91f586a0a6efe1ca08370bdac2c0285ee
Jonathanh <tajonathanh@gmail.com>
2024-06-21 14:40:06 +0200

    commentaires

--- chevaux.py
+++ chevaux.py
@@ -1,5 +1,5 @@
 # Cours hippique
-# Version très basique, sans mutex sur l'écran, sans arbitre, sans annoncer le gagant, ... ...
+# Version très basique, sans mutex sur l'écran, sans arbitre, sans annoncer le gagnant, ...
 
 # Quelques codes d'échappement (tous ne sont pas utilisés)
 CLEARSCR="\x1B[2J\x1B[;H"          #  Clear SCReen
@@ -8,7 +8,7 @@ CLEARELN = "\x1B[2K"               #  Clear Entire LiNe
 CLEARCUP = "\x1B[1J"               #  Clear Curseur UP
 GOTOYX   = "\x1B[%.2d;%.2dH"       #  ('H' ou 'f') : Goto at (y,x), voir le code
 
-DELAFCURSOR = "\x1B[K"             #  effacer après la position du curseur
+DELAFCURSOR = "\x1B[K"             #  Effacer après la position du curseur
 CRLF  = "\r\n"                     #  Retour à la ligne
 
 # VT100 : Actions sur le curseur
@@ -20,9 +20,8 @@ NORMAL = "\x1B[0m"                  #  Normal
 BOLD = "\x1B[1m"                    #  Gras
 UNDERLINE = "\x1B[4m"               #  Souligné
 
-
 # VT100 : Couleurs : "22" pour normal intensity
-CL_BLACK="\033[22;30m"                  #  Noir. NE PAS UTILISER. On verra rien !!
+CL_BLACK="\033[22;30m"                  #  Noir. NE PAS UTILISER. On ne verra rien !!
 CL_RED="\033[22;31m"                    #  Rouge
 CL_GREEN="\033[22;32m"                  #  Vert
 CL_BROWN = "\033[22;33m"                #  Brun
@@ -43,21 +42,43 @@ CL_WHITE="\033[01;37m"                  #  Blanc
 
 #-------------------------------------------------------
 import multiprocessing as mp
-import os, time,math, random, sys, ctypes, signal
+import os, time, math, random, sys, ctypes, signal
 
 # Une liste de couleurs à affecter aléatoirement aux chevaux
 lyst_colors=[CL_WHITE, CL_RED, CL_GREEN, CL_BROWN , CL_BLUE, CL_MAGENTA, CL_CYAN, CL_GRAY,
              CL_DARKGRAY, CL_LIGHTRED, CL_LIGHTGREEN,  CL_LIGHTBLU, CL_YELLOW, CL_LIGHTMAGENTA, CL_LIGHTCYAN]
 
-def effacer_ecran() : print(CLEARSCR,end='')
-def erase_line_from_beg_to_curs() : print("\033[1K",end='')
-def curseur_invisible() : print(CURSOFF,end='')
-def curseur_visible() : print(CURSON,end='')
-def move_to(lig, col) : print("\033[" + str(lig) + ";" + str(col) + "f",end='')
+# Fonction pour effacer l'écran
+def effacer_ecran():
+    print(CLEARSCR, end='')
 
-def en_couleur(Coul) : print(Coul,end='')
-def en_rouge() : print(CL_RED,end='') # Un exemple !
-def erase_line() : print(CLEARELN,end='')
+# Fonction pour effacer la ligne depuis le début jusqu'au curseur
+def erase_line_from_beg_to_curs():
+    print("\033[1K", end='')
+
+# Fonction pour rendre le curseur invisible
+def curseur_invisible():
+    print(CURSOFF, end='')
+
+# Fonction pour rendre le curseur visible
+def curseur_visible():
+    print(CURSON, end='')
+
+# Fonction pour déplacer le curseur à une position donnée (ligne, colonne)
+def move_to(lig, col):
+    print("\033[" + str(lig) + ";" + str(col) + "f", end='')
+
+# Fonction pour changer la couleur du texte
+def en_couleur(Coul):
+    print(Coul, end='')
+
+# Fonction pour changer la couleur du texte en rouge (exemple)
+def en_rouge():
+    print(CL_RED, end='')
+
+# Fonction pour effacer la ligne entière
+def erase_line():
+    print(CLEARELN, end='')
 
 # La tâche d'un cheval
 def un_cheval(ma_ligne, positions, keep_running, mutex):
@@ -86,11 +107,14 @@ def un_cheval(ma_ligne, positions, keep_running, mutex):
             move_to(ma_ligne * 4 + 4, col)
             print("/\\  /\\")
         
+        # Attendre un temps aléatoire avant de faire avancer le cheval
         time.sleep(0.1 * random.randint(1, 5))
 
+    # Arrêter la course si le cheval a atteint la fin de la course
     if col >= LONGEUR_COURSE:
         keep_running.value = False
 
+# Fonction pour demander la prédiction de l'utilisateur sur le cheval gagnant
 def demander_prediction():
     while True:
         try:
@@ -102,20 +126,21 @@ def demander_prediction():
         except Exception as e:
             print(f"Erreur: {e}")
 
+# Fonction pour vérifier si la prédiction de l'utilisateur était correcte
 def verifier_prediction(prediction_utilisateur, chevaux_gagnants):
     if prediction_utilisateur in chevaux_gagnants:
         print("Vous avez choisi le bon cheval!")
     else:
         print("Dommage! Le cheval que vous avez choisi n'a pas gagné.")
 
-
 # Fonction pour l'arbitre de la course
 def arbitre(positions, keep_running, mutex):
     while True:
-        time.sleep(0.5)
+        time.sleep(0.5)  # Attendre un certain temps avant de mettre à jour l'affichage
         with mutex:
             current_positions = list(positions)
 
+        # Vérifier les positions actuelles des chevaux et déterminer les premiers et les derniers
         tied_horses = {}
         for i, pos in enumerate(current_positions):
             if pos in tied_horses:
@@ -128,6 +153,7 @@ def arbitre(positions, keep_running, mutex):
         premier_horses = [i for i, pos in enumerate(current_positions) if pos == premier]
         dernier_horses = [i for i, pos in enumerate(current_positions) if pos == dernier]
 
+        # Afficher les positions actuelles des chevaux
         with mutex:
             move_to(Nb_process * 4 + 2, 1)
             erase_line()
@@ -141,9 +167,11 @@ def arbitre(positions, keep_running, mutex):
             erase_line()
             print(f"Dernier: {', '.join(chr(ord('A') + i) for i in dernier_horses)}")
 
+        # Sortir de la boucle si la course est terminée
         if not keep_running.value:
             break
 
+    # Afficher les résultats finaux de la course
     with mutex:
         premier_horses = [i for i, pos in enumerate(current_positions) if pos == premier]
         dernier_horses = [i for i, pos in enumerate(current_positions) if pos == dernier]
@@ -160,6 +188,7 @@ def arbitre(positions, keep_running, mutex):
         erase_line()
         print(f"Dernier: {', '.join(chr(ord('A') + i) for i in dernier_horses)}")
 
+# Fonction pour gérer les interruptions par signal (ex. Ctrl+C)
 def detourner_signal(signum, stack_frame):
     move_to(Nb_process * 5 + 5, 1)
     erase_line()
@@ -187,15 +216,19 @@ if __name__ == "__main__":
 
     signal.signal(signal.SIGINT, detourner_signal)
 
+    # Lancer les processus pour chaque cheval
     for i in range(Nb_process):
         mes_process[i] = mp.Process(target=un_cheval, args=(i, positions, keep_running, mutex))
         mes_process[i].start()
+
+    # Lancer le processus pour l'arbitre
     arbitre_process = mp.Process(target=arbitre, args=(positions, keep_running, mutex))
     arbitre_process.start()
 
     move_to(Nb_process * 4 + 10, 1)
     print("Tous lancés, CTRL-C arrêtera la course ...")
 
+    # Attendre la fin de tous les processus
     for i in range(Nb_process):
         mes_process[i].join()
 
@@ -207,6 +240,7 @@ if __name__ == "__main__":
     max_position = max(current_positions)
     chevaux_gagnants = [i for i, pos in enumerate(current_positions) if pos == max_position]
 
+    # Vérifier la prédiction de l'utilisateur
     verifier_prediction(prediction_utilisateur, chevaux_gagnants)
 
     curseur_visible()
